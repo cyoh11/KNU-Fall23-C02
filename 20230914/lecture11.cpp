5e2fc2657c5859621bb6b18a1cc39ecbcf929538
Chanyoung Oh <cyoh@kongju.ac.kr>
2023-09-14 12:24:10 +0900

    ox

---
+++ 20230914/lecture11.cpp
@@ -0,0 +1,26 @@
+// 구구단 2~9단 출력
+// 5단은 출력X
+
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+	i = 2;
+	j = 1;
+
+	while (i <= 9) {
+		if (i == 5) {
+			i++;
+			continue;
+		}
+		while (j <= 9) {
+			printf("%d * %d = %d\n", i, j, i * j);
+			j++;
+		}
+		i++;
+		j = 1;
+	}
+
+	return 0;
+}
